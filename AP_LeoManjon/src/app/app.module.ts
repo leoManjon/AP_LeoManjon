bf7ebd7b7c31c79a860ba71aaf14080fbe9b43c7
leoManjon <leomanjon@gmail.com>
2022-05-20 01:39:06 -0300

    empezando el terminando el acerca de

--- AP_LeoManjon/src/app/app.module.ts
+++ AP_LeoManjon/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { LogoAPComponent } from './componets/logo-ap/logo-ap.component';
+import { SocialComponent } from './componets/social/social.component';
+import { BannerComponent } from './componets/banner/banner.component';
+import { AcerdaDeComponent } from './componets/acerda-de/acerda-de.component';
+import { ExperienciaComponent } from './componets/experiencia/experiencia.component';
+import { EducacionComponent } from './componets/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './componets/hys/hys.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcerdaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
